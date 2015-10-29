54825fd59a8795f131528d0285dcb76b237fc659
= <=>
2015-10-29 13:17:40 -0700

    no duplicate usernames

--- register.php
+++ register.php
@@ -10,20 +10,35 @@
 	$username = $_POST['username'];	
 	$password = $_POST['password'];
 
-	if ($username && $password) {	
-		$row = insertquery("INSERT INTO users (id,username,password) VALUES (NULL,:username,:password);" , 
-		array('username' => $username, 'password' => $password), 
-		$conn);
-		header('Location: ./index.php');    
+	if ($username && $password) {		
+
+		$userExistsCheck = query("SELECT * FROM users WhERE username = :username",
+			array('username' => $username ),
+			$conn);
+
+		if($userExistsCheck) {
+			echo "
+            	<script type=\"text/javascript\">
+           			alert('Username Taken')    
+            		window.location = './register_html.php'
+           		</script>";
+		}else{
+
+			$row = insertquery("INSERT INTO users (id,username,password) VALUES (NULL,:username,:password);" , 
+			array('username' => $username, 'password' => $password), 
+			$conn);
+			$_SESSION['CurrentUser'] = $username;		
+			header('Location: ./index.php');    	
+		}
 	}else{	
 		echo "
             <script type=\"text/javascript\">
            		alert('please enter username and password')    
             	window.location = './login_html.php'
-            </script>
-        ";
+            </script>";
         die();
 	}
+
 
 
 
